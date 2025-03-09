6ed929a84cf350409febff59974857cf75c7a7d5
myrizpod <ugo.almeras@gmail.com>
2025-03-09 17:03:06 +0100

    fixed map camera

--- camera.py
+++ camera.py
@@ -10,15 +10,15 @@ class Camera:
         self.yaw = 0
     
     def update(self):
-        if pygame.mouse.get_pressed()[2] and not pygame.mouse.get_just_pressed()[2]:
+        if pygame.mouse.get_pressed()[2]:
             self.inputs()
-        self.pitch %= 1000
+        else:
+            pygame.mouse.get_rel()
+        self.pitch %= 1024
         self.yaw = min(max(self.yaw,0),180)
         
     def inputs(self):
         rel = pygame.mouse.get_rel()
-        if t.dist(0,0,rel[0],rel[1])>500:
-            return
         print(rel)
         self.pitch-= rel[0]/10
         self.yaw-= rel[1]/10
